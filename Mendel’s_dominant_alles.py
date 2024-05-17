51ca1e6615a70bf0cd0a613db31a7fa5e8613562
Gul Bulbul <79763247+gbulbul@users.noreply.github.com>
2024-05-17 04:09:53 -0500

    Update Mendel’s_dominant_alles.py

--- Mendel’s_dominant_alles.py
+++ Mendel’s_dominant_alles.py
@@ -2,22 +2,22 @@ import random
 k = 2
 m = 2
 n = 2
-def duplicate(testList, n):
-    return [ele for ele in testList for _ in range(n)]
+def MultipLy(multiList, n):
+    return [ele for ele in multiList for _ in range(n)]
 def domin_all(k,h,r):
     trials = 0
     dominants = 0
     full_comb=[]
     while True:
         list_AA=['AA'] 
-        full_comb1=duplicate(list_AA,k)
+        full_comb1=MultipLy(list_AA,k)
         list_Aa=['Aa'] 
-        full_comb2=duplicate(list_Aa,h)
+        full_comb2=MultipLy(list_Aa,h)
         list_aa= ['aa'] 
-        full_comb3=duplicate(list_aa,r)
+        full_comb3=MultipLy(list_aa,r)
         full_comb=full_comb1+full_comb2+full_comb3
-        res = [(a, b) for idx, a in enumerate(full_comb) for b in full_comb[idx + 1:]]
-        trials=sum(4 for ele in res)
-        result=4*sum(ele==('AA','aa') for ele in res)+4*sum(ele==('AA','Aa') for ele in res)+ 2*sum(ele== ('Aa','aa') for ele in res)+3*sum(ele==('Aa','Aa') for ele in res) + 4*sum(ele==('AA','AA') for ele in res)
-        return res, result/trials,trials
+        pairs = [(a, b) for idx, a in enumerate(full_comb) for b in full_comb[idx + 1:]]
+        trials=sum(4 for ele in pairs)
+        result=4*sum(ele==('AA','aa') for ele in pairs)+4*sum(ele==('AA','Aa') for ele in pairs)+ 2*sum(ele== ('Aa','aa') for ele in pairs)+3*sum(ele==('Aa','Aa') for ele in pairs) + 4*sum(ele==('AA','AA') for ele in pairs)
+        return pairs, result/trials,trials
 print(domin_all(k,m,n))
